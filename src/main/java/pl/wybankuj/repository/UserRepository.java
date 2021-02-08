f50161ce46621455642f2ceef0a370f6c6ad4f17
LukaszK1983 <49955350+LukaszK1983@users.noreply.github.com>
2021-02-08 17:09:21 +0100

    Delete src/main directory

--- src/main/java/pl/wybankuj/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.wybankuj.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import pl.wybankuj.entity.User;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-}
